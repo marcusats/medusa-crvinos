e5bc50a2312e434b4f824c937172d22bad3fdfd4
Marco Aurelio Salazar <90633967+marcusats@users.noreply.github.com>
2024-10-04 14:53:11 -0400

    more migrations

--- store-server/src/loaders/extend-product-fields.ts
+++ store-server/src/loaders/extend-product-fields.ts
@@ -1,25 +1,25 @@
-// export default async function () {
-//   const imports = (await import(
-//     "@medusajs/medusa/dist/api/routes/store/products/index"
-//   )) as any
+export default async function () {
+  const imports = (await import(
+    "@medusajs/medusa/dist/api/routes/store/products/index"
+  )) as any
 
   
-//   const customAttributes = [
-//     "wine_collection",
-//     "grape_variety",
-//     "vintage",
-//     "region",
-//     "alcohol_content",
-//     "tasting_notes",
-//     "serving_temperature",
-//   ]
+  const customAttributes = [
+    "wine_collection",
+    "grape_variety",
+    "vintage",
+    "region",
+    "alcohol_content",
+    "tasting_notes",
+    "serving_temperature",
+  ]
 
-//   imports.allowedStoreProductsFields = [
-//     ...imports.allowedStoreProductsFields,
-//     ...customAttributes,
-//   ]
-//   imports.defaultStoreProductsFields = [
-//     ...imports.defaultStoreProductsFields,
-//     ...customAttributes,
-//   ]
-// }
+  imports.allowedStoreProductsFields = [
+    ...imports.allowedStoreProductsFields,
+    ...customAttributes,
+  ]
+  imports.defaultStoreProductsFields = [
+    ...imports.defaultStoreProductsFields,
+    ...customAttributes,
+  ]
+}
